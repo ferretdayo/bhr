04098cc59ca4757a65b4636bbcc084f177c52f2a
ferretdayo <ferretdayo@hotmail.co.jp>
2018-12-22 12:49:48 +0900

    a

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,13 @@
 <template>
-  <div class="about"><h1>This is an about page</h1></div>
+  <div class="about">
+    <h1>This is an {{title}} page</h1>
+  </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    title: "About"
+  })
+};
+</script>
