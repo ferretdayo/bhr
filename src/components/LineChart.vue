aeef3d8065c463e640581e40dc74c7760797259d
ferretdayo <ferretdayo@hotmail.co.jp>
2018-12-23 15:32:48 +0900

    feat: add chart

---
+++ src/components/LineChart.vue
@@ -0,0 +1,20 @@
+<script>
+import { Line, Bar, mixins } from "vue-chartjs";
+
+export default {
+  mixins: [Line, mixins.reactiveProp],
+  props: {
+    chartData: {
+      type: Object,
+      required: true
+    },
+    options: {
+      type: Object,
+      required: false
+    }
+  },
+  mounted() {
+    this.renderChart(this.chartData, this.options);
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,58 @@
 <template>
-  <HelloWorld text="やっほー"/>
+  <div>
+    <HelloWorld text="やっほー"/>
+    <LineChart :chartData="chartData" :options="options" class="line_chart"/>
+  </div>
 </template>
 
 <script>
 import HelloWorld from "../components/HelloWorld";
+import LineChart from "../components/LineChart";
 
 export default {
+  data: () => ({
+    chartData: {
+      labels: ["1月", "2月", "3月"],
+      datasets: [
+        {
+          label: "サンプル1",
+          data: [10, 5, 10]
+        },
+        {
+          label: "サンプル2",
+          data: [3, 13, 40]
+        }
+      ]
+    },
+    options: {
+      scales: {
+        yAxes: [{ ticks: { beginAtZero: true } }]
+      },
+      elements: {
+        line: {
+          tension: 0 // ベジェ曲線を無効にする
+        }
+      },
+      animation: {
+        duration: 0 // 一般的なアニメーションの時間
+      },
+      hover: {
+        animationDuration: 0 // アイテムのマウスオーバー時のアニメーションの長さ
+      },
+      responsiveAnimationDuration: 0, // サイズ変更後のアニメーションの長さ
+      responsive: true
+    }
+  }),
   components: {
-    HelloWorld
+    HelloWorld,
+    LineChart
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.line_chart {
+  width: 500px;
+  height: 500px;
+}
+</style>
